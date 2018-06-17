219312914ddf2f8d35999bbf4901b43f74e21f79
Peter Koller <peterkollerlv@live.com>
2018-06-17 18:21:17 +0200

    Add files via upload

---
+++ streetStyleCrew-dua/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'nav-menu',
+    templateUrl: './navmenu.component.html',
+    styleUrls: ['./navmenu.component.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class NavMenuComponent {
+}
